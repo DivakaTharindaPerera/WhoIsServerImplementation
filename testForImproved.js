72649569216382bc378d49c076beb010d943bbf2
DivakaTharindaPerera <108717649+DivakaTharindaPerera@users.noreply.github.com>
2025-03-01 14:01:30 +0530

    Update the test case with lmdb implementation

--- testForImproved.js
+++ testForImproved.js
@@ -2,6 +2,12 @@ const fs = require('fs');
 const path = require('path');
 const workerpool = require('workerpool');
 const writeData = require('./writeToFile');
+const {open} = require('lmdb');
+
+const lmdbDB = open({
+    path: './lmdb-data',
+    compression: true,
+});
 
 const DATA_DIR = './test_data'; 
 const POOL_SIZE = require('os').cpus().length; // Use all available CPU cores
@@ -13,8 +19,55 @@ const start = Date.now();
 const pool = workerpool.pool('./parserWorker.js', { maxWorkers: POOL_SIZE });;
 const end = Date.now();
 
+totalTime += end - start;
+
 let objects = []; 
 let completed = 0;
+
+const writeQueue = [];
+let isWriting = false;
+
+// write data to the lmdb database in batches
+function writeBatchToLMDB(batch) {
+    return new Promise((resolve, reject) => {
+        setImmediate(() => {
+            try {
+                lmdbDB.transaction(() => {
+                    batch.forEach(obj => {
+                        lmdbDB.put(obj.domainName, {
+                            status: obj.status,
+                            createdDate: obj.creationDate,
+                        });
+                    });
+                });
+
+                resolve();
+            } catch (err) {
+                console.error('Error updating LMDB:', err);
+                reject(err);
+            }
+        });
+    });
+}
+
+async function processWriteQueue() {
+    if (isWriting || writeQueue.length === 0) return;
+    isWriting = true;
+
+    try {
+        const batch = writeQueue.shift();
+        await writeBatchToLMDB(batch);
+    } catch (err) {
+        console.error('Error processing write queue:', err);
+    } finally {
+        isWriting = false;
+
+        if (writeQueue.length > 0) {
+            // process the next batch
+            processWriteQueue();
+        }
+    }
+}
 
 // use a callback function to measure the time taken to process a file
 // essential since we are reading the directory asynchronously
@@ -51,6 +104,13 @@ function readAllFiles(directory = DATA_DIR) {
 
                             if (objects.length >= 1000 || completed === totalFiles) {
                                 writeData(objects);
+
+                                writeQueue.push([...objects]);
+
+                                if (!isWriting) {
+                                    processWriteQueue();
+                                }
+
                                 objects = [];
                             }
 
